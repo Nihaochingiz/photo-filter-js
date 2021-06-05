032d314994daa308602ba976aa93a7d5822dbb85
Ilya Fofanov <66513936+Nihaochingiz@users.noreply.github.com>
2021-06-05 12:18:26 +0300

    add
    add fullscreen

--- script.js
+++ script.js
@@ -3,10 +3,41 @@ const inputs = document.querySelectorAll('.filters input')
 
 function handleUpdate() {
 	const suffix = this.dataset.sizing || '';
-		document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+	document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
+document.querySelector('.fullscreen').addEventListener('click', check);
+ document.querySelector('.fullscreen').addEventListener('click', toggleScreen);
+
+
+ function check() {
+ 	console.log('fullscreenEnabled: ');
+ 	console.log(document.fullscreenEnabled);
+ 	console.log('fullscreenElement: ');
+ 	console.dir(document.fullscreenElement);
+ }
+
+
+ function toggleScreen () {
+ 	if (document.fullscreenElement == null) {
+ 		document.documentElement.requestFullscreen();
+ 	}
+ 	else {
+
+ 		if (document.fullscreenEnabled) {
+
+ 			document.exitFullscreen();
+
+ 		}
+
+ 	}
 
 
 	inputs.forEach(input => input.addEventListener('change', handleUpdate));
 	inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+
+
+
+}
+
